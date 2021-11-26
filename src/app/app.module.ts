4cccfb556bb20a944d64b541ccb4459ec1c673cd
Fernando Santillan <ferdiego88@gmail.com>
2021-11-26 09:41:10 -0500

    crear Componente manualmente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent
+    ContadorComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule
